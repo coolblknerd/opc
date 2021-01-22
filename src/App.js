cd13a54c5f20f7398448bafe10ef5b748cd79505
coolblknerd <reginald.davis88@gmail.com>
2021-01-21 22:35:38 -0600

    Removed the original hero portion

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
-
 import Map from "./components/Map/Map";
 import TopBar from "./components/TopBar/TopBar";
 
@@ -72,44 +71,6 @@ export default function Album() {
       <CssBaseline />
       <TopBar />
       <main>
-        {/* Hero unit */}
-        <div className={classes.heroContent}>
-          <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="textPrimary"
-              gutterBottom
-            >
-              Album layout
-            </Typography>
-            <Typography
-              variant="h5"
-              align="center"
-              color="textSecondary"
-              paragraph
-            >
-              Something short and leading about the collection below—its
-              contents, the creator, etc. Make it short and sweet, but not too
-              short so folks don&apos;t simply skip over it entirely.
-            </Typography>
-            <div className={classes.heroButtons}>
-              <Grid container spacing={2} justify="center">
-                <Grid item>
-                  <Button variant="contained" color="primary">
-                    Main call to action
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button variant="outlined" color="primary">
-                    Secondary action
-                  </Button>
-                </Grid>
-              </Grid>
-            </div>
-          </Container>
-        </div>
         <Map location={location} zoomLevel={17} />
         {/* End hero unit */}
         <Container className={classes.cardGrid} maxWidth="md">
