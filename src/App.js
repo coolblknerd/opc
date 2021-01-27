b31ff3bad389b04612a13d1353bb0096e1efdcb4
coolblknerd <reginald.davis88@gmail.com>
2021-01-26 21:52:53 -0600

    Wrapped app w/ context & set-up token on component render

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // Libraries
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
@@ -11,6 +11,7 @@ import Footer from "./components/Footer/Footer";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Preferences from "./components/Preferences/Preferences";
 import Login from "./components/Login/Login";
+import AppContext from "./AppContext";
 
 const location = {
   address: "1600 Amphitheatre Parkway, Mountain View, california.",
@@ -21,36 +22,48 @@ const location = {
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function App() {
-  const [token, setToken] = useState();
+  const [token, setToken] = useState("");
 
-  // setToken(localStorage.getItem("token"));
+  const configureToken = () => {
+    return setToken(localStorage.getItem("token"));
+  };
+
+  const globalSettings = {
+    token: token,
+  };
+
+  useEffect(() => {
+    configureToken();
+  }, []);
 
   if (!token) {
     return <Login setToken={setToken} />;
   }
 
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <TopBar />
-      <BrowserRouter>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/preferences">
-            <Preferences />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/">
-            <Map location={location} zoomLevel={17} />
-            <Results cards={cards} />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-      <Footer />
-    </React.Fragment>
+    <AppContext.Provider value={globalSettings}>
+      <React.Fragment>
+        <CssBaseline />
+        <TopBar />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/preferences">
+              <Preferences />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <Map location={location} zoomLevel={17} />
+              <Results cards={cards} />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+        <Footer />
+      </React.Fragment>
+    </AppContext.Provider>
   );
 }
